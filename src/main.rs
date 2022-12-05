47e17b4079d9c88a9ea2f4a075855556ae8130ce
Henry Lefevre <lefevre@anatoscope.com>
2022-12-06 00:11:28 +0100

    puzzle 4 complete

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,74 @@
 
 use std::fs;
 
-fn find_match_char(s1: &str, s2: &str) -> char {
-    for c1 in s1.chars() {
-        for c2 in s2.chars() {
-            if c1 == c2 {
-                return c1;
-            }
-        }
-    };
+const TABLE_POW_2: [u16;9] = [1,2,4,8,16,32,64,128,256];
 
-    panic!("Something went wrong ...")
+fn to_bytes(d: u32) -> u128 {
+    return 2u128.pow(d);
 }
 
-fn score(c: char) -> u32 {
-    let n: u32 = u32::from(c);
-    if n > 96 {
-        return n - 96;
-    } else {
-        return 26 + (n - 64);
+fn into_bytes_representation(section: &str) -> u16 {
+    let mut b = 0;
+
+    for c in section.chars() {
+        if c == '.' {
+            continue;
+        }
+        let digit = c as usize - '0' as usize;
+        b = b | TABLE_POW_2[digit - 1];
     }
+
+    return b;
+}
+
+fn one_section_contains_the_other(bytes1: u128, bytes2: u128) -> bool {
+    return (bytes1 & bytes2 == bytes1) || (bytes2 & bytes1 == bytes2);
+}
+
+fn does_sections_overlap(bytes1: u128, bytes2: u128) -> bool {
+    return (bytes1 & bytes2) != 0;
 }
 
 fn main() {
-    println!("Hello, world!");
-
-    let contents = fs::read_to_string("./data/input_1")
+    let contents = fs::read_to_string("./data/input_2")
         .expect("file not found");
 
     let lines = contents.lines();
 
-    let mut total: u32 = 0;
-
+    let mut total = 0;
+    
     for line in lines {
-        if line.eq("") {
-            continue;
+        let index = line.find(',').unwrap();
+        let e1: &str = &line[..index];
+        let e2: &str = &line[index+1..];
+
+        let index = e1.find('-').unwrap();
+        let s1 = &e1[..index].parse::<u32>().unwrap();
+        let s2 = &e1[index+1..].parse::<u32>().unwrap();
+
+        let mut b1 = 0;
+
+        for s in *s1..(*s2+1) {
+            b1 = b1 | to_bytes(s - 1);
         }
 
-        let bagpack: (&str, &str) = line.split_at(line.len() / 2);
-        let pocket_1: &str = &bagpack.0;
-        let pocket_2: &str = &bagpack.1;
+        let index = e2.find('-').unwrap();
+        let s1 = &e2[..index].parse::<u32>().unwrap();
+        let s2 = &e2[index+1..].parse::<u32>().unwrap();
 
-        let c: char = find_match_char(pocket_1, pocket_2);
-        let n: u32 = score(c);
+        let mut b2 = 0;
 
-        /*
-        let chars: [char;8] = ['a','b','y','z','A','B','Y','Z'];
-        for c in chars { 
-            let u: u32 = get_score(c);
-            println!(">>> '{}' = {}", c, u);
+        for s in *s1..(*s2+1) {
+            b2 = b2 | to_bytes(s - 1);
         }
-        */
 
-        total += n;
+        //println!(">>> {:#011b}", b1);
+        //println!(">>> {:#011b}", b2);
+        
+        if does_sections_overlap(b1, b2) {
+            total += 1;
+        }
     }
 
-    println!(">>> {}", total);
+    println!(">>>> {}", total);
 }
